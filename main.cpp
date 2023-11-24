9558f414696ffa01309011ebd97aa9d67548d8d0
V01DZ3R0 <91835641+V01DZ3R0@users.noreply.github.com>
2023-11-24 03:01:31 +0300

    Refactoring

--- main.cpp
+++ main.cpp
@@ -33,7 +33,6 @@ void convertDialog(const std::filesystem::path &txtFilePath)
     if (inputFile.is_open() && outputFile.is_open())
     {
         std::string content((std::istreambuf_iterator<char>(inputFile)), std::istreambuf_iterator<char>());
-        // std::vector<std::string> dialogLines = extractDialogLines(content);
 
         std::vector<std::string> dialogLines;
         std::string line;
@@ -69,35 +68,34 @@ std::string formatLine(std::string line)
     // (e.g., [E:1], [STOPLIP:], [XS:jilltalk,1], [C:13], etc.)
     // You can use string manipulation functions or regular expressions for this.
     // Add the formatted dialog line to the dialogLines vector.
-    std::string formattedLine = line;
 
     // Remove [E:1] tag
-    // formattedLine = std::regex_replace(formattedLine, std::regex("\\[E:\\d+\\]"), "");
+    // line = std::regex_replace(line, std::regex("\\[E:\\d+\\]"), "");
 
     // Remove [STOPLIP:] tag
-    formattedLine = std::regex_replace(formattedLine, std::regex("\\[STOPLIP:\\]"), "");
+    line = std::regex_replace(line, std::regex("\\[STOPLIP:\\]"), "");
 
     // Remove [XS:dantalk,1] or [XS:dantalk,0] tag
 
-    formattedLine = std::regex_replace(formattedLine, std::regex("\\[XS:(\\w+)hide,(\\d+)\\]"), "\nleave $1");
-    formattedLine = std::regex_replace(formattedLine, std::regex("\\[XS:mix,1\\]"), "\nset {Dialogic.paused} = true\n");
-    formattedLine = std::regex_replace(formattedLine, std::regex("\\[XS:[^\\]]+\\]"), "");
+    line = std::regex_replace(line, std::regex("\\[XS:(\\w+)hide,(\\d+)\\]"), "\nleave $1");
+    line = std::regex_replace(line, std::regex("\\[XS:mix,1\\]"), "\nset {Dialogic.paused} = true\n");
+    line = std::regex_replace(line, std::regex("\\[XS:[^\\]]+\\]"), "");
 
     // Remove [C:15] or [C:C] tag
-    formattedLine = std::regex_replace(formattedLine, std::regex("\\[C:\\d+\\]"), "");
-    formattedLine = std::regex_replace(formattedLine, std::regex("\\[C:C\\]"), "");
-    formattedLine = std::regex_replace(formattedLine, std::regex("\\[SHAKE:\\]"), "");
-    formattedLine = std::regex_replace(formattedLine, std::regex("#"), "");
-    formattedLine = std::regex_replace(formattedLine, std::regex("\\[SHOW:\\]"), "");
+    line = std::regex_replace(line, std::regex("\\[C:\\d+\\]"), "");
+    line = std::regex_replace(line, std::regex("\\[C:C\\]"), "");
+    line = std::regex_replace(line, std::regex("\\[SHAKE:\\]"), "");
+    line = std::regex_replace(line, std::regex("#"), "");
+    line = std::regex_replace(line, std::regex("\\[SHOW:\\]"), "");
 
-    formattedLine = std::regex_replace(formattedLine, std::regex("\\[DOOR:\\]"), "\n[sound path=\"res://resources/Exported_Sounds/audiogroup_default/door.ogg\" volume=\"0.0\" bus=\"SFX\"]\n");
-    formattedLine = std::regex_replace(formattedLine, std::regex("\\[BANG:\\]"), "\n[sound path=\"res://resources/Exported_Sounds/audiogroup_default/plomamentazon.ogg\" volume=\"0.0\" bus=\"SFX\"]\n");
+    line = std::regex_replace(line, std::regex("\\[DOOR:\\]"), "\n[sound path=\"res://resources/Exported_Sounds/audiogroup_default/door.ogg\" volume=\"0.0\" bus=\"SFX\"]\n");
+    line = std::regex_replace(line, std::regex("\\[BANG:\\]"), "\n[sound path=\"res://resources/Exported_Sounds/audiogroup_default/plomamentazon.ogg\" volume=\"0.0\" bus=\"SFX\"]\n");
 
     std::regex wait_re("\\[P:(\\d+)\\]");
     std::regex show_re("\\[SHOW:(\\d+),sprite_(\\w+)\\]");
     std::smatch match;
 
-    if (std::regex_search(formattedLine, match, show_re))
+    if (std::regex_search(line, match, show_re))
     {
         std::string number_str = match[1]; // match[1] contains the number
         int number = std::stoi(number_str);
@@ -114,15 +112,15 @@ std::string formatLine(std::string line)
             number = 3;
         }
 
-        formattedLine = std::regex_replace(formattedLine, show_re, "join $2 " + std::to_string(number) + "\n");
+        line = std::regex_replace(line, show_re, "join $2 " + std::to_string(number) + "\n");
     }
 
-    if (std::regex_search(formattedLine, match, wait_re))
+    if (std::regex_search(line, match, wait_re))
     {
         std::string number_str = match[1]; // match[1] contains the number
         // float number = std::stof(number_str) / 30;
 
-        formattedLine = std::regex_replace(formattedLine, wait_re, "\n[wait time=\"" + std::to_string(std::stof(number_str) / 30) + "\"]\n");
+        line = std::regex_replace(line, wait_re, "\n[wait time=\"" + std::to_string(std::stof(number_str) / 30) + "\"]\n");
     }
-    return formattedLine;
+    return line;
 }
